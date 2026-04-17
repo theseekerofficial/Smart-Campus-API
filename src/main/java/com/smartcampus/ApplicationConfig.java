325a4c60b229af1fe13651b269639f2631451540
The Seeker (Omni) <jalithapubgboss@gmail.com>
2026-04-17 14:28:53 +0530

    feat: created ApplicationConfig file

---
+++ src/main/java/com/smartcampus/ApplicationConfig.java
@@ -0,0 +1,31 @@
+package com.smartcampus;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/api/v1")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+
+        // Resources
+        classes.add(com.smartcampus.resource.DiscoveryResource.class);
+        classes.add(com.smartcampus.resource.RoomResource.class);
+        classes.add(com.smartcampus.resource.SensorResource.class);
+
+        // Exception Mappers
+        classes.add(com.smartcampus.exception.RoomNotEmptyExceptionMapper.class);
+        classes.add(com.smartcampus.exception.LinkedResourceNotFoundExceptionMapper.class);
+        classes.add(com.smartcampus.exception.SensorUnavailableExceptionMapper.class);
+        classes.add(com.smartcampus.exception.GlobalExceptionMapper.class);
+
+        // Filters
+        classes.add(com.smartcampus.filter.LoggingFilter.class);
+
+        return classes;
+    }
+}
